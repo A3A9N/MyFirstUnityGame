d96cee674108f582f9ddba13581f511382f9bb8c
A3A9N <144899095+A3A9N@users.noreply.github.com>
2024-03-01 14:59:31 +0100

    tutorial 1 beter versie

--- My first Game/Assets/Scripts/Dance.cs
+++ My first Game/Assets/Scripts/Dance.cs
@@ -14,29 +14,17 @@ public class Example : MonoBehaviour
 
     void Start()
     {
-        //Get the Animator attached to the GameObject you are intending to animate.
+        
         m_Animator = gameObject.GetComponent<Animator>();
     }
 
     void Update()
     {
-        //Press the up arrow button to reset the trigger and set another one
+
         if (Input.GetKey(KeyCode.UpArrow))
         {
-            //Reset the "Crouch" trigger
-            m_Animator.ResetTrigger("Crouch");
-
-            //Send the message to the Animator to activate the trigger parameter named "Jump"
-            m_Animator.SetTrigger("Jump");
+            m_Animator.SetTrigger("Dance");
         }
 
-        if (Input.GetKey(KeyCode.DownArrow))
-        {
-            //Reset the "Jump" trigger
-            m_Animator.ResetTrigger("Jump");
-
-            //Send the message to the Animator to activate the trigger parameter named "Crouch"
-            m_Animator.SetTrigger("Crouch");
-        }
     }
 }
