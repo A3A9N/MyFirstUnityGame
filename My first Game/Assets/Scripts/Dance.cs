010000902be22d635bb17061e962600308f67f4d
A3A9N <144899095+A3A9N@users.noreply.github.com>
2024-03-07 14:18:32 +0100

    Mini Cube game
    a

--- My first Game/Assets/Scripts/Dance.cs
+++ My first Game/Assets/Scripts/Dance.cs
@@ -2,12 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-//Attach this script to a GameObject with an Animator component attached.
-//For this example, create parameters in the Animator and name them “Crouch” and “Jump”
-//Apply these parameters to your transitions between states
-
-//This script allows you to trigger an Animator parameter and reset the other that could possibly still be active. Press the up and down arrow keys to do this.
-
 public class Example : MonoBehaviour
 {
     Animator m_Animator;
